3aef74941f937da47aff438ab1241070267a89f0
fjodor <wolf.riepl@statistik-dresden.de>
2019-04-04 21:34:17 +0200

    First commit

---
+++ R/tsort_master.R
@@ -0,0 +1,8 @@
+# tsortmusicr: fjodor's first R package
+
+# Ideas:
+# Function to import data from tsort.info
+# Function(s) to prepare data for analysis and plotting
+# Functions to create some pre-defined ggplot2 graphs
+# Functions to create some pre-defined animations with (the new version of) gganimate
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# tsortmusicr
+Functions to work with data from http://tsort.info/ - The World's Music Charts
+
+Currently no more than a few vague ideas ... Will probably take some time to evolve ...
